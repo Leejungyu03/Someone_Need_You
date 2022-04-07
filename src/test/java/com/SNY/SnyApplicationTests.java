e6898a29d7923c7997392f3ecea27f5f6bcf7302
301-07\User <l1j1g5@naver.com>
2022-04-07 21:39:52 +0900

    초기셋팅

---
+++ src/test/java/com/SNY/SnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SNY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

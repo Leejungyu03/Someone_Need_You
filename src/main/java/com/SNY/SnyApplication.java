d43ce36299522aa047a747e88de85a086638fca7
Leejungyu03 <l1j1g4@gmail.com>
2022-04-08 18:08:30 +0900

    초기 셋팅 완료

--- src/main/java/com/SNY/SnyApplication.java
+++ src/main/java/com/SNY/SnyApplication.java
@@ -2,8 +2,10 @@ package com.SNY;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan(basePackages = "com.SNY")   // com.marobiana 패키지 아래에 있는 스프링빈들 탐색
+@SpringBootApplication    // 스프링 구동을 위한 필수 어노테이션
 public class SnyApplication {
 
 	public static void main(String[] args) {
